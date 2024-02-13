0bc2bd3c9d22780b6080b5c74dbca3f95aa32325
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-02-13 15:21:14 -0800

    initial commits

---
+++ buttonactions.go
@@ -0,0 +1,8 @@
+package frameproto
+
+const (
+	ButtonActionLink         = "link"
+	ButtonActionMint         = "mint"
+	ButtonActionPost         = "post"
+	ButtonActionPostRedirect = "post_redirect"
+)
