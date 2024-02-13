d706d012a4173b4838960609255a0ef4a93f09b7
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-02-13 10:21:48 -0800

    initial commits

---
+++ writeframeimage.go
@@ -0,0 +1,22 @@
+package frameproto
+
+import (
+	"io"
+)
+
+// WriteFrameImage will write the HTML <meta/> element for the Frame-Protocol's (i.e., Farcaster Frame's) "fc:frame:image" name-value pair.
+//
+// For example, this call:
+//
+//	var url string = "https://example.com/images/screen.png"
+//	
+//	frameproto.WriteFrameImage(writer, url)
+//
+// Would write this HTML <meta/> element:
+//
+//	<meta property="fc:frame:image" content="https://example.com/images/screen.png" />
+func WriteFrameImage(writer io.Writer, url string) {
+	const property string = MetaPropertyFrameImage
+	writeMetaPropertyContent(writer, property, url)
+}
+
