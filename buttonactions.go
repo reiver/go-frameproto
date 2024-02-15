86903a797ae2fd7e72fb9c9cd4eabf2c3cd02c0c
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-02-15 09:10:09 -0800

    initial commits

--- buttonactions.go
+++ buttonactions.go
@@ -1,5 +1,28 @@
 package frameproto
 
+// These constants are 'values' in name-value pairs used with HTML <meta /> elements by the frame-protocol, when the name is of the form "fc:frame:button:*:action".
+// I.e., when the name is: "fc:frame:button:1:action", "fc:frame:button:2:action", "fc:frame:button:3:action", or "fc:frame:button:4:action".
+//
+// For example:
+//
+//      <meta property="fc:frame:button:1:action" content="post" />
+//      <meta property="fc:frame:button:2:action" content="post_redirect" />
+//      <meta property="fc:frame:button:3:action" content="mint" />
+//      <meta property="fc:frame:button:4:action" content="link" />
+//
+// The way these would be used are:
+//
+//	frameproto.WriteFrameButton1Action(writer, frameproto.ButtonActionLink)
+//
+// And:
+//
+//	p = frameproto.AppendFrameButton2Action(p, frameproto.ButtonActionMint)
+//
+// And:
+//
+//	s = frameproto.FrameButton3Action(p, frameproto.ButtonActionPost)
+//
+// Etc.
 const (
 	ButtonActionLink         = "link"
 	ButtonActionMint         = "mint"
